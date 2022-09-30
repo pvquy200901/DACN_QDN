203a39cad23f526ade5de7631476d213feb789d0
quy <93233872+pvquy200901@users.noreply.github.com>
2022-09-30 15:13:58 +0700

    update

---
+++ BackEnd_Football/APIs/MyRole.cs
@@ -0,0 +1,34 @@
+﻿using BackEnd_Football.Models;
+
+namespace BackEnd_Football.APIs
+{
+    public class MyRole
+    {
+        public MyRole()
+        {
+        }
+
+        public async Task initAsync()
+        {
+            using (DataContext context = new DataContext())
+            {
+                SqlRole? role = context.sqlRoles!.Where(s => s.isdeleted == false && s.code.CompareTo("admin") == 0).FirstOrDefault();
+                if (role == null)
+                {
+                    SqlRole tmp = new SqlRole();
+                    tmp.ID = DateTime.Now.Ticks;
+                    tmp.code = "admin";
+                    tmp.name = "admin";
+                    tmp.des = "admin";
+                    tmp.note = "admin";
+                    tmp.isdeleted = false;
+                    context.sqlRoles!.Add(tmp);
+                }
+
+                
+
+                int rows = await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ BackEnd_Football/APIs/MyUserSystem.cs
@@ -0,0 +1,35 @@
+﻿using BackEnd_Football.Models;
+
+namespace BackEnd_Football.APIs
+{
+    public class MyUserSystem
+    {
+        public MyUserSystem()
+        { }
+
+        public async Task initAsync()
+        {
+            using (DataContext context = new DataContext())
+            {
+                SqlUserSystem? user = context.sqlUserSystems!.Where(s => s.user.CompareTo("admin") == 0).FirstOrDefault();
+                if (user == null)
+                {
+                    SqlUserSystem item = new SqlUserSystem();
+                    item.ID = DateTime.Now.Ticks;
+                    item.user = "admin";
+                    item.username = "admin";
+                    item.password = "123456";
+                    item.role = context.sqlRoles!.Where(s => s.isdeleted == false && s.code.CompareTo("admin") == 0).FirstOrDefault();
+                    item.token = "1234567890";
+                    item.des = "admin";
+                    item.phoneNumber = "123456";
+                    item.isdeleted = false;
+                    context.sqlUserSystems!.Add(item);
+                }
+
+              
+                int rows = await context.SaveChangesAsync();
+            }
+        }
+    }
+}
